9823aad4a7165052e3e225c7acaba78c5ad0aca4
Mohannd <mohandzaatreh@gmail.com>
2023-06-29 09:44:54 +0300

    Solve problem 16

---
+++ Programming-Advices/08 - Algorithms & Problem Solving Level 4/16_IncreaseDateByoneDay.cpp
@@ -0,0 +1,108 @@
+/*
+Write a problem to read Date ,
+and make function to increse date by one day
+*/
+
+/*
+wirte a programe to read Date 1 and Date 2 and check :
+1- Is it's last day in a month
+2- Is it's last month in a year
+*/
+
+#include <iostream>
+using namespace std;
+struct stDate
+{
+    short Year;
+    short Month;
+    short Day;
+};
+
+bool isLeapYear(short Year)
+{
+    return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short NumberOfDaysInAMonth(short Month, short Year)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int NumberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) : NumberOfDays[Month - 1];
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+    return (NumberOfDaysInAMonth(Date.Month, Date.Year) == Date.Day);
+}
+
+bool IsLastmonthInyear(short Month)
+{
+    return (Month == 12);
+}
+
+void IncreaseDatebyoneDay(stDate &Date)
+{
+    ((IsLastmonthInyear((Date.Month))) && (IsLastDayInMonth((Date))) ? Date.Day = 1, Date.Month = 1, Date.Year++ : ((IsLastDayInMonth((Date))) && (!IsLastmonthInyear((Date.Month))) ? Date.Day = 1, Date.Month++ : (!IsLastDayInMonth((Date))) && (!IsLastmonthInyear((Date.Month))) ? Date.Day++
+                                                                                                                                                                                                                                                                                      : Date.Day));
+}
+
+// void IncreaseDatebyoneDay(stDate &Date)
+// {
+//     if ((IsLastmonthInyear(Date.Month)) && (IsLastDayInMonth(Date)))
+//     {
+//         Date.Day = 1;
+//         Date.Month = 1;
+//         Date.Year++;
+//     }
+//     else if ((IsLastDayInMonth(Date)) && (!IsLastmonthInyear(Date.Month)))
+//     {
+//         Date.Day = 1;
+//         Date.Month++;
+//     }
+
+//     else if (!IsLastDayInMonth(Date) && !IsLastmonthInyear(Date.Month))
+//         Date.Day++;
+// }
+
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+short ReadMonth()
+{
+    short Month;
+    cout << "Please enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+short ReadYear()
+{
+    short Year;
+    cout << "Please enter a Year? ";
+    cin >> Year;
+    return Year;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+    return Date;
+}
+int main()
+{
+    stDate Date1 = ReadFullDate();
+
+    IncreaseDatebyoneDay(Date1);
+    cout << Date1.Day << "/" << Date1.Month << "/" << Date1.Year;
+
+    system("pause>0");
+    return 0;
+}
