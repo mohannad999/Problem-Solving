9d5fb75236aafcc8eae3aca9954c5d5ac6e81d44
Mohannd <mohandzaatreh@gmail.com>
2023-08-16 23:47:37 +0300

    uploud course number 12

---
+++ Programming-Advices/12 - Data Structures - Level1/06_Operations-InsertAtBeginning.cpp
@@ -0,0 +1,53 @@
+
+
+#include <iostream>
+using namespace std;
+
+// Create a node
+class Node 
+{
+public:
+    int value;
+    Node *next;
+};
+
+void InsertAtBeginning(Node* &head, int value) 
+{   //Pointer to node and when using & that mean it will use the same sent variable memory (Call By Reffrence)
+    
+    // Allocate memory to a node
+    Node * new_node = new Node();
+
+    // insert the data
+    new_node->value = value;
+    new_node->next = head;
+
+    // Move head to new node
+    head = new_node;
+   
+}
+
+// Print the linked list
+void PrintList( Node* head) 
+
+{
+    while (head != NULL) {
+        cout << head->value << " ";
+        head = head->next;
+    }
+}
+
+int main() 
+{
+    Node *head = NULL;
+
+    InsertAtBeginning(head, 1);
+    InsertAtBeginning(head, 2);
+    InsertAtBeginning(head, 3);
+    InsertAtBeginning(head, 4);
+    InsertAtBeginning(head, 5);
+
+    PrintList(head);
+
+    system("pause>0");
+
+}
