723a504e1f2148d5bcd52eec13c673ffaf75b00d
Mohannd <mohandzaatreh@gmail.com>
2023-08-16 23:39:44 +0300

    upload course number 7

---
+++ Programming-Advices/07 - Algorithms & Problem Solving Level 3/49_FindClinetByAccountNumber.cpp
@@ -0,0 +1,150 @@
+// PS#49.3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+const string ClientsFileName = "Clients.txt";
+
+struct sClient
+{
+
+    string AccountNumber;
+    string PinCode;
+    string Name;
+    string Phone;
+    double AccountBalance;
+};
+
+vector<string> SplitString(string S1, string Delim)
+{
+    vector<string> vString;
+    short pos = 0;
+    string sWord; // define a string variable  // use find() function to get the position of the delimiters
+    while ((pos = S1.find(Delim)) != std::string::npos)
+    {
+        sWord = S1.substr(0, pos); // store the word
+        if (sWord != "")
+        {
+            vString.push_back(sWord);
+        }
+        S1.erase(0, pos + Delim.length());
+    }
+    if (S1 != "")
+    {
+        vString.push_back(S1); // it adds last word of the string.
+    }
+    return vString;
+}
+
+sClient ConvertLinetoRecord(string Line, string Seperator = "#//#")
+{
+    sClient Client;
+    vector<string> vClientData;
+    vClientData = SplitString(Line, Seperator);
+    Client.AccountNumber = vClientData[0];
+    Client.PinCode = vClientData[1];
+    Client.Name = vClientData[2];
+    Client.Phone = vClientData[3];
+    Client.AccountBalance = stod(vClientData[4]); // cast string to double
+    return Client;
+}
+
+vector<sClient> LoadCleintsDataFromFile(string FileName)
+{
+    vector<sClient> vClients;
+    fstream MyFile;
+    MyFile.open(FileName, ios::in); // read Modeif (MyFile.is_open())
+    {
+        string Line;
+        sClient Client;
+        while (getline(MyFile, Line))
+        {
+            Client = ConvertLinetoRecord(Line);
+            vClients.push_back(Client);
+        }
+        MyFile.close();
+    }
+    return vClients;
+}
+
+void PrintOneClientRecord(sClient Client)
+{
+
+    // cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
+    cout << "The Following are the Client details : \n";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n"
+         << endl;
+    cout << "| " << left << setw(15) << "Accout Number";
+    cout << "| " << left << setw(10) << "Pin Code";
+    cout << "| " << left << setw(40) << "Client Name";
+    cout << "| " << left << setw(12) << "Phone";
+    cout << "| " << left << setw(12) << "Balance";
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n"
+         << endl;
+    cout << "| " << setw(15) << left << Client.AccountNumber;
+    cout << "| " << setw(10) << left << Client.PinCode;
+    cout << "| " << setw(40) << left << Client.Name;
+    cout << "| " << setw(12) << left << Client.Phone;
+    cout << "| " << setw(12) << left << Client.AccountBalance;
+
+    cout << "\n_______________________________________________________";
+    cout << "_________________________________________\n"
+         << endl;
+}
+
+bool FindClientByAccountNumber(vector<sClient> vClients, string AccountNumber)
+{
+    for (sClient Client : vClients)
+    {
+        if (Client.AccountNumber == AccountNumber)
+        {
+            PrintOneClientRecord(Client);
+            return true;
+        }
+    }
+    cout << "The Clint not found\n";
+    return false;
+}
+
+bool FindClientByAccountNumber(sClient &Client, string AccountNumber)
+{
+    vector<sClient> vClients;
+    vClients = LoadCleintsDataFromFile(ClientsFileName);
+    for (sClient C : vClients)
+    {
+        if (C.AccountNumber == AccountNumber)
+        {
+            Client = C;
+            return true;
+        }
+    }
+    return false;
+}
+
+void main()
+{
+    // vector<sClient> vClints;
+    // vClints = LoadCleintsDataFromFile(ClientsFileName);
+
+    // FindClientByAccountNumber(vClints, "A151");
+
+    sClient Client;
+
+    if (FindClientByAccountNumber(Client, "A155"))
+    {
+        PrintOneClientRecord(Client);
+    }
+    else
+    {
+
+        cout << "The Clint not found\n";
+    }
+}
